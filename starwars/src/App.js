f634cded93fe693f8550ce949252711af6fd0c2d
Sharon Mosher <sharon.a.mosher@gmail.com>
2020-07-31 23:18:25 -0400

    still progressing

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import CharacterCard from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,6 +10,19 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+const [characters, setcharacters] = useState([]);
+
+useEffect(() => {
+  axios
+  .get('http://swapi.dev/api/people')
+  .then((response) => {
+    console.log(response);
+    setcharacters(response.data)
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}, []);
 
   return (
     <div className="App">
